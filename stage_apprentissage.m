a810968d4d1526c232991191a66110eca5fb2241
Noelle FAVIER <favier.noelle@gmail.com>
2017-06-06 11:15:21 +0200

    ** Création des fichiers Matlab suivants :
    *** stage_apprentissage.m --> découverte de l'interface guide de Matlab et tests sur cet interface
    *** stage_interface.m --> fichier final à rendre au client

---
+++ stage_apprentissage.m
@@ -0,0 +1,14 @@
+%% Apprentissage de l'outil 'Guide' de Matlab 
+
+% Auteur : Noëlle FAVIER
+
+%% Clean Matlab 
+
+clear all
+close all
+clc
+
+%% 
+
+guide
+
---
+++ stage_interface.m
@@ -0,0 +1,9 @@
+%% Stage : Creation d un interface graphique avec Matlab
+
+% Auteur : Noëlle FAVIER 
+
+% Maitre de stage : Thao Dang
+
+% Autre concerné : Alexandre Rocca
+
+%% 
