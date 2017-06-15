4437ffef2d70be75897b4bf947b51791226026f9
Noelle FAVIER <favier.noelle@gmail.com>
2017-06-15 17:34:03 +0200

    * fichier pour tester les fonctions interactive_graph.m and plot_rectangles.m

--- interface/test_fonction_affichage.m
+++ interface/test_fonction_affichage.m
@@ -4,7 +4,7 @@
 % 
 % Client : Santosh Arvind ADIMOOLAM
 
-% new script to test the function to plot the 2 dimension rectangle and get 
+% Script to test the function to plot the 2 dimension rectangle and get 
 % the robustness value of the selected point
 
 %% Clean Matlab
@@ -13,6 +13,14 @@ clear all
 close all
 clc 
 
+load Experience1
 
+
+plot_rectangles(Out_exp1, 1, 2)
+% revoir cette fonction
+
+
+
+% interactive_graph(Out_exp1, 5, 8)
 
 
