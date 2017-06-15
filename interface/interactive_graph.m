113a4a5fffdd1a53c9456549f1d2f0a72c993df8
Noelle FAVIER <favier.noelle@gmail.com>
2017-06-15 17:31:12 +0200

    * Version of the code asked by Arvind to plot a rectangle with ** two dimensions ** all the points inside ** it is possible to get the robustness value with a click on a point
    However :
    *** Only one rectangle is plot (I choose for now just the first one)
    
    Corrections :
    **** good choice of color for the robustness value
    **** data are now input of the function
    
    To be done :
    ***** the user can choose which rectangle he wants to plot

--- interface/interactive_graph.m
+++ interface/interactive_graph.m
@@ -1,4 +1,10 @@
-function interactive_graph(i, j, varargin)
+function interactive_graph(Out, i, j, varargin)
+%
+% 
+% 
+% 
+% 
+
 
 if nargin == 0
   clear all
@@ -6,46 +12,55 @@ end
 
 clc
 
-fprintf(['--------------------\n', ...
-  'B-spline Lab\nPress ''H'' for help.\n', ...
-  '--------------------\n\n'])
+% fprintf(['--------------------\n', ...
+%   'B-spline Lab\nPress ''H'' for help.\n', ...
+%   '--------------------\n\n'])
 
-load Experience1
 
-global Out_exp1 colone1 colone2
+
+global colone1 colone2 values
 
 colone1 = i ;
 colone2 = j ;
-
+values = Out
 
 H = figure();
 
 
+
+% plot points in the figure
+
+
+% Définition of color used to plot
 colormap(jet_inverted)
 
-x = Out_exp1.clusters{1, 1}.pts(:,i);
-y = Out_exp1.clusters{1, 1}.pts(:,j);
-c = Out_exp1.clusters{1, 1}.vals(:)
+% selection of the points
+x = Out.clusters{1}.pts(:,i);
+y = Out.clusters{1}.pts(:,j);
+% Selection of the data for the range of the color
+c = Out.clusters{1}.vals(:)
+% plot
 scatter(x,y,[],c)
 colorbar('')
             
+% set option of graphic 
 set(H, 'MenuBar','none');
 set(H,'ToolBar','none');
 set(H, 'Renderer','OpenGL');
 hold on
 
+% set option of graphic to be able to use the mouse
 set(H,'WindowButtonMotionFcn',@MouseMove);
 set(H,'WindowButtonDownFcn',@MouseClick);
 set(H,'WindowScrollWheelFcn',@MouseScroll);
 set(H,'KeyPressFcn',@KeyPress ); 
 
- 
- global pointeurX pointeurY
-            
 disp('fenetre ouverte')
 
 function MouseMove(~,~)
 
+% function which act when we move the mouse (get the current position of
+% the mouse)
 global pointeurX pointeurY
 CurrentPoint = get(gca,'CurrentPoint');
 % CurrentPoint
@@ -56,19 +71,27 @@ pointeurY=CurrentPoint(2,2);
 
 function MouseClick(~,~)
 
-global pointeurX pointeurY Out_exp1 colone1 colone2
+% function which act when we click with the mouse :
+% in every case disp (left click) and
+    % if it's on a point -> plot robustness value
+    % if it's not on a point -> do nothing
+    
+    
+global pointeurX pointeurY colone1 colone2 values
+
+% Left click
 if strcmpi(get(gcf,'SelectionType'), 'Normal')
     disp('left click')
 
-% Left click
+
     epsilon = 0.3 ;
     for l=1:50 
-        if abs(pointeurY - Out_exp1.clusters{1, 1}.pts(l,colone2))< epsilon & ...
-                abs(pointeurX - Out_exp1.clusters{1, 1}.pts(l,colone1))< epsilon 
+        if abs(pointeurY - values.clusters{1, 1}.pts(l,colone2))< epsilon & ...
+                abs(pointeurX - values.clusters{1, 1}.pts(l,colone1))< epsilon 
             disp('robustness value of selected point :')
-            Out_exp1.clusters{1, 1}.vals(l)
-            text(Out_exp1.clusters{1, 1}.pts(l,colone1),Out_exp1.clusters{1, 1}.pts(l,colone2), ...
-            num2str(Out_exp1.clusters{1, 1}.vals(l)))
+            values.clusters{1, 1}.vals(l)
+            text(values.clusters{1, 1}.pts(l,colone1),values.clusters{1, 1}.pts(l,colone2), ...
+            num2str(values.clusters{1, 1}.vals(l)))
         else              
         end
     end
